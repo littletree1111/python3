8105ead76607f4954d0e2a1fa4ba065b738c0026
pc <pc@pc>
2021-09-25 01:09:18 -0300

    leetcode 2 add two numbers

--- leetcode21mergetwosortedlists.py
+++ leetcode21mergetwosortedlists.py
@@ -3,8 +3,26 @@ class Solution:
 		if not l1 or not l2:
 			return l1 if l1 else l2
 
+		# keep lesser value to be current working node
 		if l1.val > l2.val:
 			l1, l2 = l2, l1
 		l1.next = self.mergeTwoLists(l1.next, l2)
 
 		return l1
+
+
+		# if not l1 or not l2:
+		# 	return l1 if l1 else l2
+
+		# dummy = ListNode()
+		# anchor = dummy
+		# while l1 and l2:
+		# 	if l1.val <= l2.val:
+		# 		anchor.next = l1
+		# 		l1 = l1.next
+		# 	else:
+		# 		anchor.next = 12
+		# 		l2 = l2.next
+		# 	anchor = anchor.next
+		# anchor.next = l1 if l1 else l2
+		# return dummy.next
